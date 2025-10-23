47f08e11e9e19ab54890a2a766e61af09e2aa2d7
saheer07 <saheerchungat07@gmail.com>
2025-10-23 09:43:49 +0530

    last commit on Oct 23

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -378,7 +378,7 @@ function About() {
                 className="pt-4"
               >
                 <motion.a
-                  href="/your-resume.pdf"
+                  href="file:///C:/Users/CHUNGATH/Downloads/Saheer_c%20(1).pdf"
                   download
                   whileHover={{ scale: 1.05, y: -2 }}
                   whileTap={{ scale: 0.98 }}
